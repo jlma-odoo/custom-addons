e5cc9ea54b1e4ecc083bda8bf1d7e8e9d1cf6665
jlma-odoo <jlma@odoo.com>
2023-07-20 10:59:06 -0600

    Correción del bug de la función_check_license_plate y tarea 10

--- kawiil_registry/models/motorcycle_registry.py
+++ kawiil_registry/models/motorcycle_registry.py
@@ -8,13 +8,30 @@ class MotorcycleRegistry(models.Model):
     _description = "modulo de prueba"
     registry_number = fields.Char(required=True,default='CRM0000',readonly=True)
     vin = fields.Char(string = "Código de identificación (vin)",required=True)
-    first_name = fields.Char(string = "Nombres", required=True)
-    last_name = fields.Char(string = "Apellidos", required=True)
     picture = fields.Image(string = "Foto")
     current_mileage = fields.Float("Mileage")
     license_plate = fields.Char()
     certificate_title = fields.Binary()
     register_date = fields.Date()
+    owner_id = fields.Many2one(comodel_name="res.partner", string='Owner',ondelete='cascade')
+    email = fields.Char(related='owner_id.email')
+    phone = fields.Char(related='owner_id.phone')
+    make = fields.Char(compute='_compute_registry_data')
+    model = fields.Char(compute='_compute_registry_data')
+    year = fields.Char(compute='_compute_registry_data')
+
+    @api.depends('vin')
+    def _compute_registry_data(self):
+        self.make =''
+        self.model =''
+        self.year =''
+
+        for motorcycle in self:
+            if not motorcycle.vin == False:
+                    motorcycle.make = motorcycle.vin[0:2]
+                    motorcycle.model = motorcycle.vin[2:4]
+                    motorcycle.year = motorcycle.vin[4:6]
+
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -27,7 +44,7 @@ class MotorcycleRegistry(models.Model):
     def _check_vin_number(self):
         for motorcycle in self:
             regex = r'^[A-Z]{4}[0-9]{2}[0-9A-Z]{2}[0-9]{6}$'
-            if(re.match(regex,motorcycle.vin)==None):
+            if(motorcycle.vin== False or motorcycle.vin == '' or re.match(regex,motorcycle.vin)==None):
                 raise ValidationError('Vin format incorrect')
 
 
@@ -35,7 +52,7 @@ class MotorcycleRegistry(models.Model):
     def _check_license_plate(self):
         for motorcycle in self:
             regex = r'[A-Z][A-Z]?[A-Z]?[A-Z]?[0-9][0-9]?[0-9]?[A-Z]?[A-Z]?'
-            if(re.match(regex,motorcycle.vin)==None):
+            if(motorcycle.license_plate == False or motorcycle.license_plate == '' or re.match(regex,motorcycle.license_plate)==None):
                 raise ValidationError('License plate format incorrect')
 
 
